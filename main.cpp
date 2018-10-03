463e7aee0ba3d66a078ea4da7005cfff10366915
ClaudeL9 <43612445+ClaudeL9@users.noreply.github.com>
2018-10-02 21:55:53 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-#include <iomanip>
 //This code will calculate the banks monthly fees
 
 //What is the account balance?
 
 //How many checks have been written? This value cannot be a negative number.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+unsigned short int checks;
+int balance;
+
+
+
+
+
+    return 0;
+}
